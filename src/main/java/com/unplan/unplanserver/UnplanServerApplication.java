ca59bce5694fa04129938677d09bfed3870cac6c
young <ljy040206@gmail.com>
2026-05-18 00:09:00 +0900

    프로젝트 생성

---
+++ src/main/java/com/unplan/unplanserver/UnplanServerApplication.java
@@ -0,0 +1,13 @@
+package com.unplan.unplanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnplanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnplanServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unplan/unplanserver/UnplanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unplan.unplanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnplanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

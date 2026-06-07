61fb643dbe0bbbfd4e00d647dd93bff01f764226
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-08 06:59:45 +0900

    feat: mapping문제로 인해 @JsonProperty 추가 (#12)

--- src/main/java/com/unplan/unplanserver/domain/auth/dto/KakaoUserInfoResponseDto.java
+++ src/main/java/com/unplan/unplanserver/domain/auth/dto/KakaoUserInfoResponseDto.java
@@ -1,6 +1,7 @@
 package com.unplan.unplanserver.domain.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
@@ -8,18 +9,29 @@ public class KakaoUserInfoResponseDto {
 
     private Long id;
 
+    @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
     @Getter
     public static class KakaoAccount {
 
+        @JsonProperty("profile_nickname_needs_agreement")
         private Boolean profileNicknameNeedsAgreement;
+
         private Profile profile;
 
+        @JsonProperty("has_email")
         private Boolean hasEmail;
+
+        @JsonProperty("email_needs_agreement")
         private Boolean emailNeedsAgreement;
+
+        @JsonProperty("is_email_valid")
         private Boolean isEmailValid;
+
+        @JsonProperty("is_email_verified")
         private Boolean isEmailVerified;
+
         private String email;
 
         @Getter

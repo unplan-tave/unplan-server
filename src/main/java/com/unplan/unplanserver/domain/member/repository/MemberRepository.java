4eeb569abad55529b5ea19f931586577f40e4fe1
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-08 04:41:08 +0900

    feat: memberRepository 메서드 추가(#12)

--- src/main/java/com/unplan/unplanserver/domain/member/repository/MemberRepository.java
+++ src/main/java/com/unplan/unplanserver/domain/member/repository/MemberRepository.java
@@ -3,6 +3,11 @@ package com.unplan.unplanserver.domain.member.repository;
 import com.unplan.unplanserver.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    boolean existsByOauthId(Long oauthId);
+
+    Optional<Member> findByOauthId(Long oauthId);
 }

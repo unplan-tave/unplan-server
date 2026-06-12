35f58391b16d2d5181d8ce6e2a356242dea35ee9
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-13 02:58:22 +0900

    feat: 테스트용 accessToken 발급 api

---
+++ src/main/java/com/unplan/unplanserver/domain/auth/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.unplan.unplanserver.domain.auth.controller;
+
+import com.unplan.unplanserver.domain.auth.dto.SocialLoginResponseDto;
+import com.unplan.unplanserver.domain.auth.service.AuthService;
+import com.unplan.unplanserver.util.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+public class TestController {
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/auth/accessToken")
+    public ResponseEntity<SocialLoginResponseDto> getAccessToken(@RequestParam Long memberId){
+        return ResponseEntity.ok(new SocialLoginResponseDto(
+                jwtUtil.createJwt(memberId, "USER", true),
+                "refreshToken이 아닙니다",
+                null));
+    }
+}

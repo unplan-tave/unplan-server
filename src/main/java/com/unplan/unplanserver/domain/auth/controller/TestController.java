c60d312e345fc1915a0fc7ec5f73ec43b83733f1
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-14 18:19:13 +0900

    chore: local/dev 프로필로 토큰발급api 제한

--- src/main/java/com/unplan/unplanserver/domain/auth/controller/TestController.java
+++ src/main/java/com/unplan/unplanserver/domain/auth/controller/TestController.java
@@ -4,10 +4,12 @@ import com.unplan.unplanserver.domain.auth.dto.SocialLoginResponseDto;
 import com.unplan.unplanserver.domain.auth.service.AuthService;
 import com.unplan.unplanserver.util.JwtUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
+@Profile({"local", "dev"})
 @RestController
 public class TestController {
     private final JwtUtil jwtUtil;

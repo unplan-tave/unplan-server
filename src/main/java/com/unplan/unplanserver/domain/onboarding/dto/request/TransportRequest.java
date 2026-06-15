dd12c016b9063e658a97c851c2bfb021392cd476
young <ljy040206@gmail.com>
2026-06-16 01:12:09 +0900

    fix: 코드 리뷰 수정 (#33)

--- src/main/java/com/unplan/unplanserver/domain/onboarding/dto/request/TransportRequest.java
+++ src/main/java/com/unplan/unplanserver/domain/onboarding/dto/request/TransportRequest.java
@@ -2,10 +2,11 @@ package com.unplan.unplanserver.domain.onboarding.dto.request;
 
 import com.unplan.unplanserver.domain.onboarding.enums.TransportType;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record TransportRequest(
         @NotNull(message = "transportTypes는 필수입니다.")
-        List<TransportType> transportTypes
+        List<@NotNull(message = "transportType은 null일 수 없습니다.") TransportType> transportTypes
 ) {
 }

95bf4ff248d5439b4add0c6a2cdf3a503b3b9477
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-23 01:36:13 +0900

    feat: soft delete 어노테이션(#49)

--- src/main/java/com/unplan/unplanserver/domain/member/entity/Member.java
+++ src/main/java/com/unplan/unplanserver/domain/member/entity/Member.java
@@ -8,6 +8,8 @@ import com.unplan.unplanserver.domain.member.enums.Role;
 import jakarta.persistence.*;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.data.annotation.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -15,6 +17,8 @@ import java.time.*;
 @Entity
 @Table(name = "member")
 @Getter
+@SQLDelete(sql = "UPDATE member SET deleted_at = NOW() WHERE member_id = ?")
+@SQLRestriction("deleted_at IS NULL")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 public class Member {

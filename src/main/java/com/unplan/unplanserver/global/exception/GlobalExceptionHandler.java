f57f00c8425edcc6e1cabcef1e33b142c0aa35bd
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-27 01:40:46 +0900

    fix: handle MethodArgumentNotValidException in GlobalExceptionHandler(#61)

--- src/main/java/com/unplan/unplanserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/unplan/unplanserver/global/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.unplan.unplanserver.global.exception;
 import com.unplan.unplanserver.global.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -33,5 +34,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .internalServerError()
                 .body(ApiResponse.fail("서버 오류가 발생했습니다."));
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.fail(message));
     }
 }

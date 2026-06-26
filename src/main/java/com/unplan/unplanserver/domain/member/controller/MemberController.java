099a8122320dcb3cf99b049324d782a248c73507
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-06-27 00:03:05 +0900

    chore: 코드리뷰 반영(#59)

--- src/main/java/com/unplan/unplanserver/domain/member/controller/MemberController.java
+++ src/main/java/com/unplan/unplanserver/domain/member/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.unplan.unplanserver.domain.member.controller;
 
 import com.unplan.unplanserver.domain.member.dto.ProfileResponseDto;
 import com.unplan.unplanserver.domain.member.service.MemberService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
 
     @GetMapping("/profile")
     public ResponseEntity<ProfileResponseDto> getProfile(@AuthenticationPrincipal Long memberId) {
--- src/main/java/com/unplan/unplanserver/domain/member/service/MemberService.java
+++ src/main/java/com/unplan/unplanserver/domain/member/service/MemberService.java
@@ -1,21 +1,23 @@
 package com.unplan.unplanserver.domain.member.service;
 
-import com.unplan.unplanserver.domain.member.controller.MemberController;
 import com.unplan.unplanserver.domain.member.dto.ProfileResponseDto;
 import com.unplan.unplanserver.domain.member.entity.Member;
 import com.unplan.unplanserver.domain.member.repository.MemberRepository;
 import com.unplan.unplanserver.global.exception.CustomException;
 import com.unplan.unplanserver.global.exception.ErrorCode;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MemberService {
-    private MemberRepository memberRepository;
-    @Transactional
+    private final MemberRepository memberRepository;
+    @Transactional(readOnly = true)
     public ProfileResponseDto getProfile(Long memberId) {
+        if (memberId == null) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
+        }
         Member member = memberRepository.findById(memberId).orElseThrow(
                 ()->new CustomException(ErrorCode.MEMBER_NOT_FOUND)
         );

1c1541dea68de27ebb8a71504c620a14edb724a2
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-07-20 21:59:41 +0900

    feat: 컨디션 기록이 없는 경우 분기처리(#111)

--- src/main/java/com/unplan/unplanserver/domain/schedule/service/ScheduleService.java
+++ src/main/java/com/unplan/unplanserver/domain/schedule/service/ScheduleService.java
@@ -539,6 +539,9 @@ public class ScheduleService {
         ConditionTag conditionTag = ConditionTag.fromLabel(dailyRecord.conditionTag());
         DailyMessage dailyMessage = DailyMessage.fromConditionTag(conditionTag);
         String message = dailyMessage.getPrescription() + "\n" + dailyMessage.getSuggestion();
+        if (!dailyRecord.isEnergyRecorded() && !dailyRecord.isSleepRecorded()) {
+            message = "컨디션을 기록하고 나에게 딱 맞는 일정을 추천 받아보세요!";
+        }
         return new DailyMessageResponseDto(
                 date,
                 dailyMessage.getConditionTag(),

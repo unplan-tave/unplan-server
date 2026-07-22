3d76a44ddd08dc42ef46a98feb1ab48ff51501f2
한근형 <165651118+hangeunhyeong@users.noreply.github.com>
2026-07-23 02:18:14 +0900

    task: 스웨거 문서 작성(#168)

--- src/main/java/com/unplan/unplanserver/domain/member/controller/MemberController.java
+++ src/main/java/com/unplan/unplanserver/domain/member/controller/MemberController.java
@@ -3,23 +3,28 @@ package com.unplan.unplanserver.domain.member.controller;
 import com.unplan.unplanserver.domain.member.dto.GetProfileResponseDto;
 import com.unplan.unplanserver.domain.member.dto.UpdateProfileRequestDto;
 import com.unplan.unplanserver.domain.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Member", description = "회원 정보 조회 및 수정 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원 정보 조회", description = "로그인한 사용자의 정보와 온보딩 완료 여부를 조회합니다")
     @GetMapping("/profile")
     public ResponseEntity<GetProfileResponseDto> getProfile(@AuthenticationPrincipal Long memberId) {
         return ResponseEntity.ok(memberService.getProfile(memberId));
     }
 
+    @Operation(summary = "회원 정보 변경", description = "로그인한 사용자의 프로필 정보를 변경합니다")
     @PatchMapping("/profile")
     public ResponseEntity<Void> updateProfile(@AuthenticationPrincipal Long memberId, @RequestBody @Valid UpdateProfileRequestDto requestDto) {
         memberService.updateProfile(memberId, requestDto);
--- src/main/java/com/unplan/unplanserver/domain/setting/controller/SettingController.java
+++ src/main/java/com/unplan/unplanserver/domain/setting/controller/SettingController.java
@@ -6,6 +6,8 @@ import com.unplan.unplanserver.domain.setting.dto.EmptyTimeSettingRequestDto;
 import com.unplan.unplanserver.domain.setting.dto.EmptyTimeSettingResponseDto;
 import com.unplan.unplanserver.domain.setting.service.SettingService;
 import com.unplan.unplanserver.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,29 +15,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Setting", description = "알림 설정 및 비어있는 시간 설정 API")
 @RestController
 @RequestMapping("/member-settings")
 @RequiredArgsConstructor
 public class SettingController {
     private final SettingService settingService;
+    @Operation(summary = "빈 시간 추천 설정 변경", description = "설정 > 추천 개인화 설정 > 일정 추천 기준 화면에서 추천 조건 관련 설정들을 변경합니다")
     @PatchMapping("/empty-time")
     public ResponseEntity<ApiResponse<EmptyTimeSettingResponseDto>> updateEmptyTimeRecommendSetting(@AuthenticationPrincipal Long memberId, @RequestBody EmptyTimeSettingRequestDto requestDto) {
         EmptyTimeSettingResponseDto responseDto = settingService.updateSetting(memberId, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(responseDto));
     }
 
+    @Operation(summary = "빈 시간 추천 설정 조회", description = "설정 > 추천 개인화 설정 > 일정 추천 기준 화면에서 추천 조건 관련 설정들을 조회합니다")
     @GetMapping("/empty-time")
     public ResponseEntity<ApiResponse<EmptyTimeSettingResponseDto>> getEmptyTimeRecommendSetting(@AuthenticationPrincipal Long memberId) {
         EmptyTimeSettingResponseDto responseDto = settingService.getSetting(memberId);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(responseDto));
     }
 
+    @Operation(summary = "알림 설정 변경", description = "설정 탭의 알림 설정을 변경합니다.")
     @PatchMapping("/alarm-setting")
     public ResponseEntity<ApiResponse<AlarmSettingResponseDto>> updateAlarmSetting(@AuthenticationPrincipal Long memberId, @RequestBody AlarmSettingRequestDto requestDto) {
         AlarmSettingResponseDto responseDto = settingService.updateAlarmSetting(memberId, requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(responseDto));
     }
 
+    @Operation(summary = "알림 설정 조회", description = "설정 화면의 사용자가 설정한 알림 설정 목록을 조회합니다.")
     @GetMapping("/alarm-setting")
     public ResponseEntity<ApiResponse<AlarmSettingResponseDto>> getAlarmSetting(@AuthenticationPrincipal Long memberId) {
         return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success(settingService.getAlarmSetting(memberId)));
